18fc129eee4d34e9c9684cafa6d4d898e9cd992a
Dakshin Priya <dakshinpriya.ts@foodhub.com>
2026-01-29 13:20:07 +0530

    Modularized order screen

--- app/order.tsx
+++ app/order.tsx
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { router } from 'expo-router';
 import { useDispatch } from 'react-redux';
 import { clearCart } from '../src/redux/cart/cart.actions';
+import { styles } from '../src/styles/OrderPlacedScreen.styles';
 
 export default function OrderPlacedScreen() {
   const dispatch = useDispatch();
@@ -24,37 +25,3 @@ export default function OrderPlacedScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  heading: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    color: '#ff6347',
-    textAlign: 'center',
-  },
-  button: {
-    backgroundColor: '#ff6347',
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-    alignItems: 'center',
-    elevation: 3,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
